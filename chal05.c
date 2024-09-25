860e912cc432aed25674482c69f86a10f0b5208a
salma hamdi <saalmahm@gmail.com>
2024-09-25 14:42:33 +0100

    some changes

--- chal05.c
+++ chal05.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
 
 int main() {
-int a,v,r;
+long long a, r;
+ int v;
 printf("entrer l\'anné : \n");
-scanf("%d",&a);
+scanf("%lld",&a);
 printf("si tu veut transformer en mois entrer \"1\" ,et si en jours entrer \"2\", et si en heures entrer \"3\" , et si en minutes entrer \"4\" , et si en secondes entrer \"5\" :\n");
 scanf("%d",&v);
 switch(v){
     case 1 :
     r=a*12;
-    printf("%d mois",r);
+    printf("%lld mois",r);
     break;
     case 2:
     r=a*365;
-    printf("%d jours",r);
+    printf("%lld jours",r);
     break;
     case 3:
     r=a*365*24;
-    printf("%d heures",r);
+    printf("%lld heures",r);
     break;
     case 4:
     r=a*356*1440;
-    printf("%d minutes",r);
+    printf("%lld minutes",r);
     break;
     case 5:
     r=a*365*86400;
-    printf("%d secondes",r);
+    printf("%lld secondes",r);
 }
     return 0;
 }
